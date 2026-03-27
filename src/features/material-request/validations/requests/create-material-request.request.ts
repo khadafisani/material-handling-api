95eb5d426825ad585d83d525f49f41a155f30d09
khadafisani <khadafisani89@gmail.com>
2026-03-27 23:10:55 +0700

    Add status to material request validation

--- src/features/material-request/validations/requests/create-material-request.request.ts
+++ src/features/material-request/validations/requests/create-material-request.request.ts
@@ -1,7 +1,17 @@
 import * as Joi from "joi";
+import MaterialRequestStatusEnum, {
+  getMaterialRequestStatusEnums,
+} from "../../enums/material-request-status.enum";
+
+const materialRequestStatusEnum = getMaterialRequestStatusEnums().map(
+  (status) => +status.id,
+);
 
 export const createMaterialRequestSchema = Joi.object({
   title: Joi.string().required(),
   date: Joi.date().required(),
   note: Joi.string().optional().allow(null),
+  status: Joi.number()
+    .valid(...materialRequestStatusEnum)
+    .default(MaterialRequestStatusEnum.REQUESTED),
 });
