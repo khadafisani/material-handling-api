43fceea27018e22f1b8d58559150049f4783bee7
khadafisani <khadafisani89@gmail.com>
2026-03-27 19:02:02 +0700

    Initial commit

---
+++ src/features/auth/validations/requests/register.request.ts
@@ -0,0 +1,33 @@
+import * as Joi from "joi";
+import { JoiException } from "src/cores/helpers/joi-exception.helper";
+import { User } from "src/features/user/entities/user.entity";
+
+export const registerSchema = Joi.object({
+  name: Joi.string().required(),
+  username: Joi.string()
+    .allow("", null)
+    .external(async (value, helper) => {
+      if (value) {
+        const user = await User.findOne({
+          where: { username: value },
+        });
+
+        if (user) {
+          throw JoiException.handle("Username already exists", helper);
+        }
+      }
+      return value;
+    }),
+  email: Joi.string()
+    .required()
+    .external(async (value, helper) => {
+      const user = await User.findOne({
+        where: { email: value },
+      });
+      if (user) {
+        throw JoiException.handle("Email already exists", helper);
+      }
+      return value;
+    }),
+  password: Joi.string().min(8),
+}).options({ abortEarly: false });
