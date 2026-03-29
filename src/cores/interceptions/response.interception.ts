9aa96f1ad2bb6c5550fb6191e35eed3ae9bc40e8
khadafisani <khadafisani89@gmail.com>
2026-03-30 03:17:06 +0700

    Update readme.md

--- src/cores/interceptions/response.interception.ts
+++ src/cores/interceptions/response.interception.ts
@@ -3,15 +3,15 @@ import {
   ExecutionContext,
   Injectable,
   NestInterceptor,
-} from '@nestjs/common';
-import { map, Observable } from 'rxjs';
+} from "@nestjs/common";
+import { map, Observable } from "rxjs";
 
 @Injectable()
 export class Response<T> implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
       map((data) => ({
-        statusCode: data.statusCode,
+        status_code: data.status_code,
         message: data.message,
         data: data.data,
       })),
